9b95bcb5849da3144f14ab70e815a0bf21fe63ce
maharoz <mahruj66@gmail.com>
2019-06-04 17:39:06 +0600

    Updates

---
+++ precache-manifest.6c8de141f55f59e78bd109ffb1626f47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e32aebf2a6750d10a9f67d0f37e4bd",
+    "url": "/React-Seasons/index.html"
+  },
+  {
+    "revision": "8b2852b05cb6e3312f27",
+    "url": "/React-Seasons/static/css/main.25ecf40b.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/React-Seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "8b2852b05cb6e3312f27",
+    "url": "/React-Seasons/static/js/main.25b79804.chunk.js"
+  },
+  {
+    "revision": "0600fa9a65e81ca79971",
+    "url": "/React-Seasons/static/js/runtime~main.8f278869.js"
+  }
+]);
